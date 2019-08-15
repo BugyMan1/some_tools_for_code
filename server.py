af75e385fcaf7444b3f8ad042838c6f3d00083bf
Yulian Camilo Chavarriaga Restrepo <yulianchavarriaga@SETIAD.COM.CO>
2019-08-15 15:17:38 -0500

    Add CORS

--- server.py
+++ server.py
@@ -2,9 +2,11 @@ from environment import Env
 from flask import Flask, jsonify
 from change_ctrl import ChangeCtrl
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 api.add_resource(ChangeCtrl,'/changectrl')
 api.add_resource(Env,'/environment')
